bbe261177f42ebfc4d2dc85db743041be5d81e56
Sonwanseo <sonwanseo@gmail.com>
2021-04-06 11:27:29 +0900

    <navigation/RouterConfig.tsx> default routing setting

---
+++ src/navigation/RouterConfig.tsx
@@ -0,0 +1,25 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import {
+  SIGN_IN,
+  SIGN_UP_FORM,
+  SIGN_UP_VERIFICATION,
+  SIGN_UP_PROFILE,
+} from "./CONSTANTS";
+
+const RouterConfig = () => {
+  return (
+    <>
+      <Switch>
+        <Redirect exact path="/" to={SIGN_IN} />
+        <Route exact path={SIGN_IN} />
+        <Route exact path={SIGN_UP_FORM} />
+        <Route exact path={SIGN_UP_VERIFICATION} />
+        <Route exact path={SIGN_UP_PROFILE} />
+
+        <Route path="*" />
+      </Switch>
+    </>
+  );
+};
+
+export default RouterConfig;
