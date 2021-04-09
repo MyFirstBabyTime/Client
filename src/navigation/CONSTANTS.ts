4cfa07bcb8a8bbb56cd1a93ca6c3e91ed6d7766d
Sonwanseo <sonwanseo@gmail.com>
2021-04-09 15:08:54 +0900

    <navigation/CONTSANTS.ts> create sign-up path constant

--- src/navigation/CONSTANTS.ts
+++ src/navigation/CONSTANTS.ts
@@ -1,5 +1,6 @@
 export const SIGN_IN = "/sign-in";
 
+export const SIGN_UP = "/sign-up";
 export const SIGN_UP_FORM = "/sign-up/form";
 export const SIGN_UP_VERIFICATION = "/sign-up/verification";
 export const SIGN_UP_PROFILE = "/sign-up/profile";
