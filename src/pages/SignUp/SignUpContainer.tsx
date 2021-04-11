8d85ea046d5cd0f7b88b8b4d866dd96d0db93dd2
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 21:41:48 +0900

    <pages/SignUp/SignUpContainer.tsx> make sign up one page

--- src/pages/SignUp/SignUpContainer.tsx
+++ src/pages/SignUp/SignUpContainer.tsx
@@ -1,6 +1,19 @@
 import { FC } from "react";
+import styled from "styled-components";
+import { Background } from "../../components/Authentication/Background";
 import { SignUpView } from "./SignUpView";
 
+const SContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
 export const SignUpContainer: FC = () => {
-  return <SignUpView />;
+  return (
+    <SContainer>
+      <Background />
+      <SignUpView />
+    </SContainer>
+  );
 };
