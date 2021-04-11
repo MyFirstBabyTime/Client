e8dba172b39310fee1391d290ae50924376e9a2e
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 21:45:38 +0900

    <pages/SignUp/SignUpView.tsx> make sign up one page

--- src/pages/SignUp/SignUpView.tsx
+++ src/pages/SignUp/SignUpView.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
-import { InputForm } from "../../components/Authentication/InputForm";
-import { PagePoint } from "../../components/Authentication/PagePoint";
-import { SubmitButton } from "../../components/Authentication/SubmitButton";
 import { Title } from "../../components/Authentication/Title";
+import SignUpForm from "./SignUpForm";
+import SignUpValidation from "./SignUpValidation";
+import SignUpProfile from "./SignUpProfile";
 
 const SContainer = styled.div`
   width: 36vw;
@@ -15,39 +15,25 @@ const SContainer = styled.div`
   box-sizing: border-box;
 `;
 
-const SInputFormWrapper = styled.div`
-  width: 24.2vw;
-  height: 34.5vh;
-  margin-top: 7.5vh;
-  margin-bottom: 10.9vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
 export const SignUpView: FC = () => {
+  const [pageNum, setPageNum] = useState<number>(1);
+
+  const onIncreasePageNum = () => {
+    setPageNum((prev) => prev + 1);
+  };
+
   return (
     <SContainer>
       <Title>회원가입</Title>
-      <SInputFormWrapper>
-        <InputForm
-          type="text"
-          title="아이디"
-          placeholder="4자리 이상 입력하세요"
-        />
-        <InputForm
-          type="password"
-          title="비밀번호"
-          placeholder="특수문자 포함 6글자 이상 입력하세요"
-        />
-        <InputForm
-          type="password"
-          title="비밀번호 확인"
-          placeholder="비밀번호를 확인하세요"
-        />
-      </SInputFormWrapper>
-      <SubmitButton text="다음" />
-      <PagePoint position={1} />
+      {pageNum === 1 ? (
+        <SignUpForm onIncreasePageNum={onIncreasePageNum} />
+      ) : pageNum === 2 ? (
+        <SignUpValidation onIncreasePageNum={onIncreasePageNum} />
+      ) : pageNum === 3 ? (
+        <SignUpProfile onIncreasePageNum={onIncreasePageNum} />
+      ) : (
+        ""
+      )}
     </SContainer>
   );
 };
