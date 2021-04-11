0e0963baa0c6764400c885eb80f982f64f7c44b3
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 21:43:24 +0900

    <pages/SignUp/SignUpForm/SignUpFormView.tsx> make detail page for sign up

---
+++ src/pages/SignUp/SignUpForm/SignUpFormView.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { InputForm } from "../../../components/Authentication/InputForm";
+import { PagePoint } from "../../../components/Authentication/PagePoint";
+import { SubmitButton } from "../../../components/Authentication/SubmitButton";
+
+const SContainer = styled.div`
+  width: 36vw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SInputFormWrapper = styled.div`
+  width: 24.2vw;
+  height: 34.5vh;
+  margin-top: 7.5vh;
+  margin-bottom: 10.9vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+interface Props {
+  onIncreasePageNum: () => void;
+}
+
+export const SignUpFormView: FC<Props> = ({ onIncreasePageNum }) => {
+  return (
+    <SContainer>
+      <SInputFormWrapper>
+        <InputForm
+          type="text"
+          title="아이디"
+          placeholder="4자리 이상 입력하세요"
+        />
+        <InputForm
+          type="password"
+          title="비밀번호"
+          placeholder="특수문자 포함 6글자 이상 입력하세요"
+        />
+        <InputForm
+          type="password"
+          title="비밀번호 확인"
+          placeholder="비밀번호를 확인하세요"
+        />
+      </SInputFormWrapper>
+      <SubmitButton text="다음" onClick={onIncreasePageNum} />
+      <PagePoint position={1} />
+    </SContainer>
+  );
+};
