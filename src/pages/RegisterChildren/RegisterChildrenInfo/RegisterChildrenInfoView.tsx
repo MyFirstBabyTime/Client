d60454f36a6309b68fe1b42dfe752424383979db
Sonwanseo <sonwanseo@gmail.com>
2021-04-13 13:48:57 +0900

    <RegisterChildrenInfoView> create RegisterChildrenInfoView

---
+++ src/pages/RegisterChildren/RegisterChildrenInfo/RegisterChildrenInfoView.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { InputForm } from "../../../components/Authentication/InputForm";
+import { SubmitButton } from "../../../components/Authentication/SubmitButton";
+import { WarningText } from "../../../components/Authentication/WarningText";
+import { PagePoint } from "../../../components/Authentication/PagePoint";
+
+const SContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SInputFormWrapper = styled.div`
+  width: 24.2vw;
+  height: 34.5vh;
+  margin-top: 7.5vh;
+  margin-bottom: 10.9vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+interface Props {
+  onIncreasePageNum: () => void;
+}
+
+export const RegisterChildrenInfoView: FC<Props> = ({ onIncreasePageNum }) => {
+  return (
+    <SContainer>
+      <SInputFormWrapper>
+        <InputForm
+          type="text"
+          title="이름"
+          placeholder="아이의 이름을 입력하세요"
+        />
+      </SInputFormWrapper>
+      <SubmitButton text="다음" onClick={onIncreasePageNum} />
+      <WarningText />
+      <PagePoint position={1} end={2} />
+    </SContainer>
+  );
+};
