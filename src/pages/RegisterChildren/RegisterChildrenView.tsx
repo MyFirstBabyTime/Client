330d2ad7ef8a87cc7b9a9219597f3ae08d92fd29
Sonwanseo <sonwanseo@gmail.com>
2021-04-18 16:43:16 +0900

    <RegisterChildrenView.tsx> set BackButton compnent

--- src/pages/RegisterChildren/RegisterChildrenView.tsx
+++ src/pages/RegisterChildren/RegisterChildrenView.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import RegisterChildrenInfo from "./RegisterChildrenInfo";
 import RegisterChildrenProfile from "./RegisterChildrenProfile";
 import { Title } from "../../components/Authentication/Title";
+import { BackButton } from "../../components/Authentication/BackButton";
+import { SIGN_UP } from "../../navigation/CONSTANTS";
 
 const SContainer = styled.div`
   width: 36vw;
@@ -11,7 +13,7 @@ const SContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 14.8vh;
+  padding-top: 4.2vh;
   box-sizing: border-box;
 `;
 
@@ -20,8 +22,13 @@ export const RegisterChildrenView: FC = () => {
   const [pageNum, setPageNum] = useState<number>(1);
 
   useEffect(() => {
+    if (pageNum === 0) history.push(SIGN_UP);
     if (pageNum === 3) history.push("/main");
   }, [pageNum, history]);
+
+  const onDecreasePageNum = () => {
+    setPageNum((prev) => prev - 1);
+  }
 
   const onIncreasePageNum = () => {
     setPageNum((prev) => prev + 1);
@@ -29,6 +36,7 @@ export const RegisterChildrenView: FC = () => {
 
   return (
     <SContainer>
+      <BackButton onClick={onDecreasePageNum} />
       <Title>내 아이 등록하기</Title>
       {pageNum === 1 ? (
         <RegisterChildrenInfo onIncreasePageNum={onIncreasePageNum} />
