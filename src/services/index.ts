cd1cbece4a4d2225b6b428196961e788690c99c8
Sonwanseo <sonwanseo@gmail.com>
2021-04-21 08:16:20 +0900

    <services/index.ts> export api funcs

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,3 @@
-export { createParentAccount } from "./signUp/signUpService";
+export { sendCertifyCode } from "./signUp/signUpValidation/signUpValidationService";
+export { getCertification } from "./signUp/signUpValidation/signUpValidationService";
+export { createParentAccount } from "./signUp/signUpValidation/signUpValidationService";
