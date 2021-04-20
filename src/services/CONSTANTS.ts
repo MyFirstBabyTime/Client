33e238d75d19f2086359a89ed1a67fbbc4aba564
Sonwanseo <sonwanseo@gmail.com>
2021-04-21 08:10:04 +0900

    <CONSTANTS.ts> export api CONSTANTS

--- src/services/CONSTANTS.ts
+++ src/services/CONSTANTS.ts
@@ -1,1 +1,5 @@
+export const SEND_CERTIFY_CODE = (phoneNumber: string) =>
+  `/phones/phone-number/${phoneNumber}/certify-code`;
+export const GET_CERTIFICATION = (phoneNumber: string) =>
+  `/phones/phone-number/${phoneNumber}/certification`;
 export const CREATE_PARENT_ACCOUNT = () => "/parents";
