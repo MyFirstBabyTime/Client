57e1ffac84cd657bfa1cd61bab1bf774be687ee8
Sonwanseo <sonwanseo@gmail.com>
2021-04-23 11:43:56 +0900

    <SignUpFormView.tsx> set props & onChangeFunc

--- src/pages/SignUp/SignUpForm/SignUpFormView.tsx
+++ src/pages/SignUp/SignUpForm/SignUpFormView.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 import styled from "styled-components";
 import { InputForm } from "../../../components/Authentication/InputForm";
 import { PagePoint } from "../../../components/Authentication/PagePoint";
@@ -23,30 +23,43 @@ const SInputFormWrapper = styled.div`
 `;
 
 interface Props {
-  onIncreasePageNum: () => void;
+  onChangeFormData: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClickNextBtn: () => void;
+  idError: boolean;
+  pwError: boolean;
+  pwConfirmError: boolean;
 }
 
-export const SignUpFormView: FC<Props> = ({ onIncreasePageNum }) => {
+export const SignUpFormView: FC<Props> = ({ onChangeFormData, onClickNextBtn, idError, pwError, pwConfirmError }) => {
   return (
     <SContainer>
       <SInputFormWrapper>
         <InputForm
           type="text"
           title="아이디"
+          name="id"
+          isError={idError}
           placeholder="4자리 이상 입력하세요"
+          onChange={onChangeFormData}
         />
         <InputForm
           type="password"
           title="비밀번호"
+          name="pw"
+          isError={pwError}
           placeholder="특수문자 포함 6글자 이상 입력하세요"
+          onChange={onChangeFormData}
         />
         <InputForm
           type="password"
           title="비밀번호 확인"
+          name="pwConfirm"
+          isError={pwConfirmError}
           placeholder="비밀번호를 확인하세요"
+          onChange={onChangeFormData}
         />
       </SInputFormWrapper>
-      <SubmitButton text="다음" onClick={onIncreasePageNum} />
+      <SubmitButton text="다음" onClick={onClickNextBtn} />
       <PagePoint position={2} end={3} />
     </SContainer>
   );
