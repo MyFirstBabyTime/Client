4a5b8c4792c4f9880884ad60c3225de57939fda5
Sonwanseo <sonwanseo@gmail.com>
2021-04-25 23:32:16 +0900

    <CONSTANTS.ts> create SIGN_IN Constant

--- src/services/CONSTANTS.ts
+++ src/services/CONSTANTS.ts
@@ -1,5 +1,7 @@
-export const SEND_CERTIFY_CODE = (phoneNumber: string) =>
+export const SIGN_UP_SEND_CERTIFY_CODE = (phoneNumber: string) =>
   `/phones/phone-number/${phoneNumber}/certify-code`;
-export const GET_CERTIFICATION = (phoneNumber: string) =>
+export const SIGN_UP_GET_CERTIFICATION = (phoneNumber: string) =>
   `/phones/phone-number/${phoneNumber}/certification`;
-export const CREATE_PARENT_ACCOUNT = () => "/parents";
+export const SIGN_UP_CREATE_PARENT_ACCOUNT = () => "/parents";
+
+export const SIGN_IN = () => "/login/parent";
