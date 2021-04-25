aee716ae5c066b9f8e0285723764bd5239bc3116
Sonwanseo <sonwanseo@gmail.com>
2021-04-25 23:29:42 +0900

    <useSignInUseCase/index.ts> create useSignInUseCase custom hook

---
+++ src/hooks/useCase/useSignInUseCase/index.ts
@@ -0,0 +1,51 @@
+import { signIn } from "../../../services";
+import { useSignIn } from "../../domain/useSignIn";
+
+const useSignInUseCase = () => {
+  const {
+    state: { signInForm, signInIdError, signInPwError },
+    setState: { setSignInForm, setSignInIdError, setSignInPwError },
+  } = useSignIn();
+
+  const signInUseCase = () => {
+    signIn({ id: signInForm.id, pw: signInForm.pw }).then(
+      (res) => {
+        localStorage.setItem("token", res?.token ?? "");
+        Promise.resolve(res);
+      },
+      (err) => {
+        switch (err.response.status) {
+          case 400:
+            alert("올바르지 않은 형식입니다.");
+            break;
+          case 409:
+            switch (err.response.data.code) {
+              case -131:
+                alert("존재하지 않는 계정입니다.");
+                setSignInIdError(true);
+                setSignInPwError(true);
+                break;
+              case -132:
+                alert("올바르지 않은 비밀번호입니다.");
+                setSignInPwError(true);
+                break;
+              default:
+                break;
+            }
+            break;
+          default:
+            break;
+        }
+        Promise.reject(err);
+      }
+    );
+  };
+
+  return {
+    state: { signInForm, signInIdError, signInPwError },
+    setState: { setSignInForm, setSignInIdError, setSignInPwError },
+    useCase: { signInUseCase },
+  };
+};
+
+export default useSignInUseCase;
