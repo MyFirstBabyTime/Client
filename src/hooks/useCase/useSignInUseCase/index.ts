4dde025f46ea36d0c10a15c0e77a3d2c304d253f
Sonwanseo <sonwanseo@gmail.com>
2021-05-14 10:56:59 +0900

    <useSignInUseCase/index.ts> setUserUuid after success of signIn

--- src/hooks/useCase/useSignInUseCase/index.ts
+++ src/hooks/useCase/useSignInUseCase/index.ts
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { signIn } from "../../../services";
 import { useSignIn } from "../../domain/useSignIn";
+import { UserContext } from '../../../contexts/UserContext';
 
 const useSignInUseCase = () => {
+  const { setUuid } = useContext(UserContext);
   const {
     state: { signInForm, signInIdError, signInPwError },
     setState: { setSignInForm, setSignInIdError, setSignInPwError },
@@ -10,6 +13,7 @@ const useSignInUseCase = () => {
   const signInUseCase = () => {
     signIn({ id: signInForm.id, pw: signInForm.pw }).then(
       (res) => {
+        setUuid(res?.uuid ?? "");
         localStorage.setItem("token", res?.token ?? "");
         Promise.resolve(res);
       },
