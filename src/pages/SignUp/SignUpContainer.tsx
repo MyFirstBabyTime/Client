5d90c8acfcebaf3f278a783f6caf7e9b1fdabe00
Sonwanseo <sonwanseo@gmail.com>
2021-05-14 11:06:09 +0900

    <SignUpContainer.tsx> move logic to Container

--- src/pages/SignUp/SignUpContainer.tsx
+++ src/pages/SignUp/SignUpContainer.tsx
@@ -1,7 +1,9 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
+import { useHistory } from "react-router";
 import styled from "styled-components";
 import { Background } from "../../components/Authentication/Background";
-import SignUpContextProvider from "../../contexts/SignUpContext";
+import SignUpContextProvider, { SignUpContext } from "../../contexts/SignUpContext";
+import { REGISTER_CHILDREN, SIGN_IN } from "../../navigation/CONSTANTS";
 import { SignUpView } from "./SignUpView";
 
 const SContainer = styled.div`
@@ -11,11 +13,19 @@ const SContainer = styled.div`
 `;
 
 export const SignUpContainer: FC = () => {
+  const history = useHistory();
+  const { pageNum, onDecreasePageNum } = useContext(SignUpContext);
+
+  useEffect(() => {
+    if (pageNum === 0) history.push(SIGN_IN);
+    if (pageNum === 4) history.push(REGISTER_CHILDREN);
+  }, [pageNum, history]);
+
   return (
     <SignUpContextProvider>
       <SContainer>
         <Background />
-        <SignUpView />
+        <SignUpView onDecreasePageNum={onDecreasePageNum} pageNum={pageNum} />
       </SContainer>
     </SignUpContextProvider>
   );
