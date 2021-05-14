e8f16bf5a2d64aae1decc8e0a6b17fec74b1e732
Sonwanseo <sonwanseo@gmail.com>
2021-05-14 11:03:54 +0900

    <RegisterChildrenInfoContainer.tsx> create Container for logic

---
+++ src/pages/RegisterChildren/RegisterChildrenInfo/RegisterChildrenInfoContainer.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { RegisterChildrenInfoView } from "./RegisterChildrenInfoView";
+
+export const RegisterChildrenInfoContainer: FC = () => {
+  return (
+    <RegisterChildrenInfoView />
+  )
+}
