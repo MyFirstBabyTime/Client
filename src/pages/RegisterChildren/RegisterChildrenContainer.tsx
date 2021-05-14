2efa484d63a6a35ce40ef3cf9fc993a6f1b90529
Sonwanseo <sonwanseo@gmail.com>
2021-05-14 10:58:33 +0900

    <RegisterChildrenContainer.tsx> move logic to Container

--- src/pages/RegisterChildren/RegisterChildrenContainer.tsx
+++ src/pages/RegisterChildren/RegisterChildrenContainer.tsx
@@ -1,7 +1,11 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
 import { Background } from "../../components/Authentication/Background";
 import styled from "styled-components";
 import { RegisterChildrenView } from "./RegisterChildrenView";
+import UserContextProvider from "../../contexts/UserContext";
+import { useHistory } from "react-router";
+import { SignUpContext } from "../../contexts/SignUpContext";
+import { SIGN_UP } from "../../navigation/CONSTANTS";
 
 const SContainer = styled.div`
   width: 100%;
@@ -10,10 +14,20 @@ const SContainer = styled.div`
 `;
 
 export const RegisterChildrenContainer: FC = () => {
+  const history = useHistory();
+  const { onDecreasePageNum, pageNum } = useContext(SignUpContext);
+
+  useEffect(() => {
+    if (pageNum === 0) history.push(SIGN_UP);
+    if (pageNum === 3) history.push("/main");
+  }, [pageNum, history]);
+
   return (
-    <SContainer>
-      <Background />
-      <RegisterChildrenView />
-    </SContainer>
+    <UserContextProvider>
+      <SContainer>
+        <Background />
+        <RegisterChildrenView onDecreasePageNum={onDecreasePageNum} pageNum={pageNum} />
+      </SContainer>
+    </UserContextProvider>
   );
 };
