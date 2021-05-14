f82ddbb4fccd845f2a8a9b1609f53d0b515fc593
Sonwanseo <sonwanseo@gmail.com>
2021-05-14 11:05:52 +0900

    <RegisterChildrenView.tsx> move logic to Container

--- src/pages/RegisterChildren/RegisterChildrenView.tsx
+++ src/pages/RegisterChildren/RegisterChildrenView.tsx
@@ -1,11 +1,9 @@
-import { FC, useState, useEffect } from "react";
-import { useHistory } from "react-router";
+import { FC } from "react";
 import styled from "styled-components";
 import RegisterChildrenInfo from "./RegisterChildrenInfo";
 import RegisterChildrenProfile from "./RegisterChildrenProfile";
 import { Title } from "../../components/Authentication/Title";
 import { BackButton } from "../../components/Authentication/BackButton";
-import { SIGN_UP } from "../../navigation/CONSTANTS";
 
 const SContainer = styled.div`
   width: 36vw;
@@ -17,31 +15,20 @@ const SContainer = styled.div`
   box-sizing: border-box;
 `;
 
-export const RegisterChildrenView: FC = () => {
-  const history = useHistory();
-  const [pageNum, setPageNum] = useState<number>(1);
+interface Props {
+  onDecreasePageNum: () => void;
+  pageNum: number;
+}
 
-  useEffect(() => {
-    if (pageNum === 0) history.push(SIGN_UP);
-    if (pageNum === 3) history.push("/main");
-  }, [pageNum, history]);
-
-  const onDecreasePageNum = () => {
-    setPageNum((prev) => prev - 1);
-  }
-
-  const onIncreasePageNum = () => {
-    setPageNum((prev) => prev + 1);
-  };
-
+export const RegisterChildrenView: FC<Props> = ({ onDecreasePageNum, pageNum }) => {
   return (
     <SContainer>
       <BackButton onClick={onDecreasePageNum} />
       <Title>내 아이 등록하기</Title>
       {pageNum === 1 ? (
-        <RegisterChildrenInfo onIncreasePageNum={onIncreasePageNum} />
+        <RegisterChildrenInfo />
       ) : pageNum === 2 ? (
-        <RegisterChildrenProfile onIncreasePageNum={onIncreasePageNum} />
+        <RegisterChildrenProfile />
       ) : (
         ""
       )}
