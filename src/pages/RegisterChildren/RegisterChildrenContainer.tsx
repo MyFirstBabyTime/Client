b31c040937fb523083957f0449816e81956fd2b7
Sonwanseo <sonwanseo@gmail.com>
2021-05-31 21:56:10 +0900

    <RegisterChildrenContainer.tsx> move context logic inside of provider

--- src/pages/RegisterChildren/RegisterChildrenContainer.tsx
+++ src/pages/RegisterChildren/RegisterChildrenContainer.tsx
@@ -1,11 +1,8 @@
-import { FC, useContext, useEffect } from "react";
+import { FC } from "react";
 import { Background } from "../../components/Authentication/Background";
 import styled from "styled-components";
 import { RegisterChildrenView } from "./RegisterChildrenView";
-import UserContextProvider from "../../contexts/UserContext";
-import { useHistory } from "react-router";
-import { SignUpContext } from "../../contexts/SignUpContext";
-import { SIGN_UP } from "../../navigation/CONSTANTS";
+import RegisterChildrenContextProvider from "../../contexts/RegisterChildrenContext";
 
 const SContainer = styled.div`
   width: 100%;
@@ -14,20 +11,12 @@ const SContainer = styled.div`
 `;
 
 export const RegisterChildrenContainer: FC = () => {
-  const history = useHistory();
-  const { onDecreasePageNum, pageNum } = useContext(SignUpContext);
-
-  useEffect(() => {
-    if (pageNum === 0) history.push(SIGN_UP);
-    if (pageNum === 3) history.push("/main");
-  }, [pageNum, history]);
-
   return (
-    <UserContextProvider>
-      <SContainer>
-        <Background />
-        <RegisterChildrenView onDecreasePageNum={onDecreasePageNum} pageNum={pageNum} />
-      </SContainer>
-    </UserContextProvider>
+      <RegisterChildrenContextProvider>
+        <SContainer>
+          <Background />
+          <RegisterChildrenView />
+        </SContainer>
+      </RegisterChildrenContextProvider>
   );
 };
