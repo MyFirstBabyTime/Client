a213e97bd3886d33ddd00bbacbb0db07f4727b50
Sonwanseo <sonwanseo@gmail.com>
2021-05-31 21:59:02 +0900

    <RegisterChildrenView.tsx> move context logic inside of provider

--- src/pages/RegisterChildren/RegisterChildrenView.tsx
+++ src/pages/RegisterChildren/RegisterChildrenView.tsx
@@ -1,9 +1,12 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
 import styled from "styled-components";
 import RegisterChildrenInfo from "./RegisterChildrenInfo";
 import RegisterChildrenProfile from "./RegisterChildrenProfile";
 import { Title } from "../../components/Authentication/Title";
 import { BackButton } from "../../components/Authentication/BackButton";
+import { useHistory } from "react-router";
+import { RegisterChildrenContext } from "../../contexts/RegisterChildrenContext";
+import { SIGN_UP } from "../../navigation/CONSTANTS";
 
 const SContainer = styled.div`
   width: 36vw;
@@ -15,12 +18,15 @@ const SContainer = styled.div`
   box-sizing: border-box;
 `;
 
-interface Props {
-  onDecreasePageNum: () => void;
-  pageNum: number;
-}
+export const RegisterChildrenView: FC = () => {
+  const history = useHistory();
+  const { pageNum, onDecreasePageNum } = useContext(RegisterChildrenContext);
 
-export const RegisterChildrenView: FC<Props> = ({ onDecreasePageNum, pageNum }) => {
+  useEffect(() => {
+    if (pageNum === 0) history.push(SIGN_UP);
+    if (pageNum === 3) history.push("/main");
+  }, [pageNum, history]);
+
   return (
     <SContainer>
       <BackButton onClick={onDecreasePageNum} />
